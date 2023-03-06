0be3f0f0f601860038bf3a33626821b8a0b71b12
Yelleti Krishna <krishna.yelleti@publicissapient.com>
2023-03-06 14:39:35 +0530

    Shopping cart with redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card';
+import Badge from './Badge';
+
+const App = () => {
+  return (
+    <div>
+        <Badge />
+        <Card />
+    </div>
+  )
+}
+
+export default App
